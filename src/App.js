92e4cabad82058fe4669d6e074eef04575d085da
Admin <Admin@LAPTOP-TUUFVBB6>
2021-05-17 09:59:27 +0300

    test x1

--- src/App.js
+++ src/App.js
@@ -8,20 +8,42 @@ import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import HeaderX from "./Header";
 import Plop from './Plop';
-import { Container, Row,Col } from 'react-bootstrap';
-
+import {Container, Row, Col, Navbar} from 'react-bootstrap';
+import './css/videoCssFile.css'
+import cyberPunk2077 from './files/cyberPs4.jpg';
 
 function App() {
   return (
     <div className="App">
         <HeaderX ></HeaderX>
-        <div className="container">
-            <div className="card-deck">
 
-                <Plop></Plop>
-                <Plop></Plop>
-                <Plop></Plop>
-                <Plop></Plop>
+
+        <div className="video-container">
+            <iframe  className="videoFrame" id="myVideo" frameborder="0" src="https://www.youtube.com/embed/UjxS9ciNlII?controls=0&autoplay=1&mute=1&showinfo=0&loop=1"
+                    frameBorder="0" allowFullScreen></iframe>
+            <div id="vidtop-content">
+                <div className="vid-info-image">
+                    <img src={cyberPunk2077} width="40%" height="60%" className="d-inline-block align-top" alt="" loading="lazy"/>
+
+                </div>
+
+                <div className="vid-info">
+                    <h1>Cyberpunk 2077</h1>
+                    <p>
+                        Cyberpunk 2077 is a 2020 action role-playing video game developed and published by CD Projekt.
+                        The story takes place in Night City, an open world set in the Cyberpunk universe.
+                        Players assume the first-person perspective of a customisable mercenary known as V,
+                        who can acquire skills in hacking and machinery with options for melee and ranged combat.
+                    </p>
+                            <a href="/500/Use-YouTube-Videos-as-Fullscreen-Web-Page-Backgrounds">Full article</a>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div className="container">
+            <div className="row">
                 <Plop></Plop>
             </div>
         </div>
--- src/Plop.js
+++ src/Plop.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 import axios from "axios";
 import fifa from "./files/fifa21X.png";
 import ps4Icon from "./files/ps4-icon.png";
-import {Collapse} from 'react-collapse';
+import {Collapse , } from 'react-collapse';
 import gameCss from './gameCss.css';
 import * as Icon from 'react-bootstrap-icons';
+import {Accordion, Button, Card , ListGroup} from "react-bootstrap";
 
 class Plop extends Component {
 
@@ -26,41 +27,41 @@ class Plop extends Component {
 
     render(){
         return(
-            <div className=" child">
-                {this.state.cards.map((card) => (
-                    <div className="row">
-                        <div className="card cardSize" id="accordion">
-                            <div className="card-img">
-                                <img src={fifa} className="center2 pedlTop5"/>
-                            </div>
+            <div className="col">
+                <div className="card-group">
+                    {this.state.cards.map((card) => (
+                        <Accordion className="pedl5">
+                            <div className="card cardSize" id="accordion">
+                                <div className="card-img">
+                                    <img src={fifa} className="center2 pedlTop5"/>
+                                </div>
 
-                            <div className="pedlTop4">
-                                <span className="gold">{card.price}</span>
-                                <img src={ps4Icon} className="iconImgPos"/>
-                            </div>
+                                <div className="pedlTop4">
+                                    <div className="cardPricePos gold">{card.price}</div>
+                                    <img src={ps4Icon} className="iconImgPos"/>
+                                </div>
 
-                            <div className="card-content">
-                                <p style={{padding: '3px;'}}>{card.shortMessage}</p>
-                            </div>
+                                <div className="card-content">
+                                    <p style={{padding: '3px;'}}>{card.shortMessage}</p>
+                                </div>
 
-                            <div>
-                                <button
-                                    className="btn btn-primary btn-circle cardButtonSize"
-                                    onClick={() => this.setState({ open: !this.state.open})}>
+                                <Accordion.Toggle as={Button} eventKey="1" className="btn btn-primary btn-circle cardButtonSize">
                                     <Icon.ChevronDown />
-                                </button>
-                            </div>
+                                </Accordion.Toggle>
 
-                            <Collapse isOpened={this.state.open}>
-                                <p style={{padding: '25px;', borderTop: '1px solid silver'}}>
-                                    <img src={ps4Icon} style={{padding: '5px;', width: '60px'}}/>
-                                    {card.fullMessage}
-                                </p>
-                            </Collapse>
-                        </div>
-                    </div>
-                    )
-                )}
+                                <Accordion.Collapse eventKey="1">
+                                    <ListGroup>
+
+                                        <ListGroup.Item action> No style </ListGroup.Item>
+                                        <ListGroup.Item action> Primary </ListGroup.Item>
+                                        <ListGroup.Item action >Secondary </ListGroup.Item>
+                                    </ListGroup>
+                                </Accordion.Collapse>
+                            </div>
+                        </Accordion>
+                        )
+                    )}
+                </div>
             </div>
         );
     }
